4c783bdc64d76311f36c61d83f49de718f08ff5a
Indrajitsinh <168441585+indrajitatodariya@users.noreply.github.com>
2025-06-28 00:18:34 +0530

    firstday2

--- Frontend/e-commerce/src/components/categories.jsx
+++ Frontend/e-commerce/src/components/categories.jsx
@@ -1,11 +1,14 @@
-
+import tv from '../assets/tv.jpg'
+import fashion from '../assets/style.jpg'
+import mobile from '../assets/mobile.jpg'
+import groce from '../assets/groce.jpg'
 function Categories(){
     return <>
     <div className="flex justify-center align-center m-5">
         <div><div className="card bg-base-100 w-46 shadow-sm">
   <figure className="px-10 pt-10">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      src={groce}
       alt="Shoes"
       className="rounded-xl" />
   </figure>
@@ -16,7 +19,7 @@ function Categories(){
         <div><div className="card bg-base-100 w-46 shadow-sm">
   <figure className="px-10 pt-10">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      src={mobile}
       alt="Shoes"
       className="rounded-xl" />
   </figure>
@@ -27,7 +30,7 @@ function Categories(){
         <div><div className="card bg-base-100 w-46 shadow-sm">
   <figure className="px-10 pt-10">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      src={fashion}
       alt="Shoes"
       className="rounded-xl" />
   </figure>
@@ -35,10 +38,10 @@ function Categories(){
     <h2 className="card-title">Fashion</h2>
   </div>
 </div></div>
-        <div><div className="card bg-base-100 w-46 shadow-sm">
+        <div><div className="card bg-base-100 w-47 shadow-sm">
   <figure className="px-10 pt-10">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      src={tv}
       alt="Shoes"
       className="rounded-xl" />
   </figure>
