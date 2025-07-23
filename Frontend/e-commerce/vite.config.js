f1487f3a40c1a7bccc16c859595792f86f82eb53
Indrajitsinh <168441585+indrajitatodariya@users.noreply.github.com>
2025-07-23 11:55:47 +0530

    tailwind change

--- Frontend/e-commerce/vite.config.js
+++ Frontend/e-commerce/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react({
       jsxRuntime: 'automatic', // ✅ Enables JSX without needing `import React`
-    }),
+    }),tailwindcss(),
   ],
   resolve: {
     alias: {
