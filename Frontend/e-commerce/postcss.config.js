325551524af8909e254058fbb88b7623f3b22e3a
Indrajitsinh <168441585+indrajitatodariya@users.noreply.github.com>
2025-07-23 11:12:24 +0530

    logs solve

--- Frontend/e-commerce/postcss.config.js
+++ Frontend/e-commerce/postcss.config.js
@@ -1,6 +1,7 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
+// postcss.config.js
 export default {
-  plugins: [tailwindcss(), autoprefixer()],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
